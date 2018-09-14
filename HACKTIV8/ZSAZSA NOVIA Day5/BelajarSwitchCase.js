fd12af1f1f5c56dfd5f59301cc91164988a1a6f7
ZNK1994 <zsazsa259@gmail.com>
2018-09-14 19:00:49 +0700

    add file

---
+++ HACKTIV8/ZSAZSA NOVIA Day5/BelajarSwitchCase.js
@@ -0,0 +1,22 @@
+var tombolTV = 1
+switch(tombolTV) {
+  case 1: { 
+    console.log('matikan TV');
+    break; 
+  }
+  case 2: {
+    console.log('kecilin suaranya!');
+    break;
+  }
+  case 3: {
+    console.log('gedein suaranya!');
+    break;
+  }
+  case 4: {
+    console.log('bisukan');
+    break;
+  }
+  default : {
+    console.log('gak ngapa2in');
+  }
+}
