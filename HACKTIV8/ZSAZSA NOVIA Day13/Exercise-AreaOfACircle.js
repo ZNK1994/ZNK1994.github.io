327812255fa1992a180a40de0707ffe9dc609d0e
ZNK1994 <zsazsa259@gmail.com>
2018-10-02 11:24:29 +0700

    add file

---
+++ HACKTIV8/ZSAZSA NOVIA Day13/Exercise-AreaOfACircle.js
@@ -0,0 +1,13 @@
+function square(number) {
+  // Your code here
+  
+  var kali = number * number;
+  return kali
+}
+function pi () {
+  // Your code here
+  return Math.PI
+}
+
+var area = Math.round(pi() * square(7));
+console.log(area); // 154
